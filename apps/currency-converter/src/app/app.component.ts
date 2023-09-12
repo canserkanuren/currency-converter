21de22ee54ada542f7e58443cf381fc73e449a10
Can Serkan UREN <canserkan.uren@gmail.com>
2023-09-12 19:17:32 +0000

    feat: initialize converter

--- apps/currency-converter/src/app/app.component.ts
+++ apps/currency-converter/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'currency-converter-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'currency-converter';
-}
+export class AppComponent {}
--- apps/currency-converter/src/app/app.config.ts
+++ apps/currency-converter/src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
-  withEnabledBlockingInitialNavigation,
+  withEnabledBlockingInitialNavigation
 } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideAnimations()
+  ]
 };
